656505d61547a3efaedb53ed02a5fd831b8da596
Eleomar Dorneles <82125143+oeleomar@users.noreply.github.com>
2024-02-12 17:29:17 -0400

    ok

--- src/task/dto/create-task.dto.ts
+++ src/task/dto/create-task.dto.ts
@@ -1,1 +1,23 @@
-export class CreateTaskDto {}
+import { IsDate, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateTaskDto {
+  @IsString()
+  @IsNotEmpty({ message: 'Nome Obrigatório' })
+  name: string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'Descrição Obrigatória' })
+  description: string;
+
+  @IsDate()
+  due_date: Date = new Date();
+
+  @IsString()
+  status: 'pendente' | 'fazendo' | 'concluido' | 'cancelado';
+
+  @IsString()
+  priority: 'baixa' | 'media' | 'alta';
+
+  @IsString()
+  project_id: string;
+}
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,26 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Task } from './entities/task.entity';
 
 @Injectable()
 export class TaskService {
+  constructor(
+    @InjectRepository(Task) private taskRepository: Repository<Task>,
+  ) {}
+
   create(createTaskDto: CreateTaskDto) {
     return 'This action adds a new task';
   }
 
   findAll() {
-    return `This action returns all task`;
+    try {
+      return this.taskRepository.find({ relations: ['project'] });
+    } catch (err) {
+      throw new BadRequestException();
+    }
   }
 
-  findOne(id: number) {
+  findOne(id: string) {
     return `This action returns a #${id} task`;
   }
 
-  update(id: number, updateTaskDto: UpdateTaskDto) {
+  update(id: string, updateTaskDto: UpdateTaskDto) {
     return `This action updates a #${id} task`;
   }
 
-  remove(id: number) {
+  remove(id: string) {
     return `This action removes a #${id} task`;
   }
 }
