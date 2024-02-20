c899ba1a4cc2f5686cc79a414b1bce0d006d8a81
Eleomar Dorneles <82125143+oeleomar@users.noreply.github.com>
2024-02-20 10:41:39 -0400

    🐞 fix(Module My-tasks):
    Changing the route

--- src/my-tasks/my-tasks.module.ts
+++ src/my-tasks/my-tasks.module.ts
@@ -2,9 +2,18 @@ import { TaskModule } from './task/task.module';
 import { ProjectModule } from './project/project.module';
 import { TagsModule } from './tags/tags.module';
 import { Module } from '@nestjs/common';
+import { RouterModule } from '@nestjs/core';
 
 @Module({
-  imports: [TaskModule, ProjectModule, TagsModule],
-  exports: [],
+  imports: [
+    TaskModule,
+    ProjectModule,
+    TagsModule,
+    RouterModule.register([
+      { path: 'my-tasks', module: TaskModule },
+      { path: 'my-tasks', module: ProjectModule },
+      { path: 'my-tasks', module: TagsModule },
+    ]),
+  ],
 })
 export class MyTasksModule {}
